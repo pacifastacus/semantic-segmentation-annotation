3426cb7bfb4515515d5609a81c1d4b9d184c5c4d
pacifastacus <palkovics.denes@gmail.com>
2021-02-27 11:22:29 +0100

    Dataframe class thinned
    Only support false-colored annotation
    colormap.txt added also

--- dataframe.py
+++ dataframe.py
@@ -11,30 +11,24 @@ class Dataframe:
     """
 
     def __init__(self, img_path,
-                 img_mode="bgr",
                  gt_dirname=None,
-                 gt_ext='.npy',
-                 gt_mode="numpy",
-                 gt_autosave=False):
+                 gt_autosave=False,
+                 colormap="colormap.txt"):
         """
         :param img_path: image filename
-        :param img_mode: how the image read from file: "rgb","rgba","bgr","gray" (default: "bgr")
         :param gt_dirname: the folder name of gts. If given, the ground-truth folder will be set to this,
                         else it will be <image dir>_gt/
-        :param gt_ext: file extension of ground-truth, eg. ".bmp", ".png", ".npy", ".h5"
-        :param gt_mode: ground-truth representation mode: "rgb","gray","numpy" (default: "numpy")
         :param gt_autosave: if True save gt on Dataframe init, update and destroy (default is False)
         """
         self._img = None  # Image
-        self._img_mode = img_mode
         self._gt = np.array(list())   # Ground-truth
-        self._gt_mode = gt_mode
         self._img_fname = None  # Image filename w/o path
         self._img_ext = None  # Image file extension
         self._img_dir = None  # Image directory (absolute path)
         self._gt_fname = None  # GT filename w/o path
-        self._gt_ext = gt_ext
         self._gt_dir = None  # Ground-truth directory (absolute path)
+        self._gt_ext = ".png"
+        self._colors = []
         self._gt_autosave = gt_autosave
 
         # Init image
@@ -53,20 +47,28 @@ class Dataframe:
         else:
             self._gt_dir = os.path.join(os.path.basename(self._img_dir), gt_dirname)
         if os.path.exists(self._gt_dir):
-            gt_path = os.path.join(self._gt_dir,self._gt_fname)
+            gt_path = os.path.join(self._gt_dir, self._gt_fname)
             if os.path.exists(gt_path) and os.path.isfile(gt_path):
                 print("Existing ground truth loaded")
-                if self._gt_mode == "numpy":
-                    self._gt = np.load(gt_path)
-                else:
-                    self._gt = cv2.imread(gt_path)
+                self._gt = cv2.imread(gt_path)
+
             else:
                 print("New ground truth created")
-                self._gt = np.zeros(self._img.shape[:2], dtype=np.uint8)  # Create a new empty map
+                self._gt = np.zeros(self._img.shape, dtype=np.uint8)  # Create a new empty map
         else:
             os.mkdir(self._gt_dir)
             if self._gt_autosave:
                 self.save_gt()
+
+        #Load colormap
+        with open(colormap,"r") as color_file:
+            for line in color_file:
+                r, g, b = line.split()
+                r = int(r)
+                g = int(g)
+                b = int(b)
+                self.colors.append([r,g,b])
+
 
     def __del__(self):
         if self._gt_autosave:
@@ -108,23 +110,16 @@ class Dataframe:
 
     def save_gt(self):
         filename = os.path.join(self._gt_dir, self._gt_fname)
-        #### Switch-case construct ####
-        def save_gray():
-            grayim = np.stack((self._gt,)*3,axis=-1)
-            cv2.imwrite(filename,grayim)
-        def save_rgb():
-            cv2.imwrite(filename,self._gt)
-        def save_np():
-            np.save(filename,self._gt)
+        cv2.imwrite(filename, self._gt)
 
-        switch = {
-            "gray" : save_gray,
-            "rgb" : save_rgb,
-            "numpy": save_np
-        }
-        #### End of Switch-case construct ####
-        switch[self._gt_mode]()
+    def label2color(self, label):
+        return self._colors[label]
 
+    def color2label(self, rgb):
+        try:
+            return self._colors.index(list(rgb))
+        except ValueError:
+            return -1
 
 if __name__ == "__main__":
     df = Dataframe("test_data/img/1_cam-image_array_.jpg",gt_autosave=False)
